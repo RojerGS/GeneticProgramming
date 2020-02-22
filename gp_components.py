b91c1c155830857a5c31766c35705eaa6de2094a
Rodrigo Girão Serrão <rodrigogiraoserrao@gmail.com>
2020-02-22 14:28:26 +0000

    Renamed file.

--- gp_components.py
+++ gp_components.py
@@ -1,3 +1,28 @@
+from typing import List
+from random import choice
+
+from interfaces import ASTNode, Function, Terminal
+
+def random_tree_full(
+    depth: int, functions: List[Function], terminals: List[Terminal]
+) -> ASTNode:
+    """Creates a random tree with full depth specified by the argument.
+    
+    Uses the given lists of functions and terminals to build the tree.
+    """
+
+    if depth == 1:
+        return choice(terminals)
+
+    else:
+        func = choice(functions)
+        arity = func.get_arity()
+        children = []
+        for _ in range(arity):
+            children.append(random_tree_full(depth - 1, functions, terminals))
+
+        return func(children)
+
 class Simulation(object):
     """Class to hold a whole genetic programming simulation."""
 
@@ -23,4 +48,4 @@ class Simulation(object):
         
         while self.current_generation < self.max_generations:
             self.next_generation()
-            self.current_generations += 1
+            self.current_generation += 1
