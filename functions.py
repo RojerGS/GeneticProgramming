982e5cf973a9c6846bcee74053247b9b927a22de
Rodrigo Girão Serrão <rodrigogiraoserrao@gmail.com>
2020-02-22 14:33:46 +0000

    Pre define some functions

---
+++ functions.py
@@ -0,0 +1,20 @@
+from typing import List
+from interfaces import BinaryFunction, ASTNode
+
+class Max(BinaryFunction):
+    """Returns the maximum of its two children."""
+    def __init__(self, children: List[ASTNode]):
+        super().__init__(self, children)
+
+    def evaluate(self, context, *args):
+        self.evaluate_children(context, *args)
+        return max(self.children_values)
+
+class Min(BinaryFunction):
+    """Returns the minimum of its two children."""
+    def __init__(self, children: List[ASTNode]):
+        super().__init__(self, children)
+
+    def evaluate(self, context, *args):
+        self.evaluate_children(context, *args)
+        return min(self.children_values)
