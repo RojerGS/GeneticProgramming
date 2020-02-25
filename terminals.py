2efd9b8df313576d4522f4f0929f01ebd612b871
RojerGS <rodrigogiraoserrao@gmail.com>
2020-02-25 11:24:54 +0000

    Inherit from Int and FloatTerminal parent classes

--- terminals.py
+++ terminals.py
@@ -1,21 +1,19 @@
 from random import random, randint
 from interfaces import Terminal
 
-class Zero(Terminal):
+class Zero(IntTerminal):
     """The 0 constant terminal."""
     def __init__(self):
         super().__init__()
         self.value = 0
 
-class One(Terminal):
+class One(IntTerminal):
     """The 1 constant terminal."""
     def __init__(self):
-        pass
+        super().__init__()
+        self.value = 1
 
-    def evaluate(self, *args) -> int:
-        return 1
-
-class IntConstant(Terminal):
+class IntConstant(IntTerminal):
     """A latent constant integer.
     
     When initialized, this terminal assumes a fixed value chosen
@@ -24,15 +22,12 @@ class IntConstant(Terminal):
     def __init__(self, min: int = 0, max: int = 1):
         self.value = randint(min, max)
 
-    def evaluate(self, *args) -> int:
-        return self.value
-
 def int_constant_factory(min: int = 0, max: int = 1) -> IntConstant:
     """Creates an IntConstant class with the specified range."""
         
     return lambda: IntConstant(min, max)
 
-class FloatConstant(Terminal):
+class FloatConstant(FloatTerminal):
     """A latent constant float.
 
     When initialized, this terminal assumes a fixed value chosen
