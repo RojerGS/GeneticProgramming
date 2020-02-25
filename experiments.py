665898bad13c7f7be7880e82d46f196dcf120c46
RojerGS <rodrigogiraoserrao@gmail.com>
2020-02-25 10:51:05 +0000

    Rename file and test evaluation

---
+++ experiments.py
@@ -0,0 +1,12 @@
+from interfaces import *
+from terminals import *
+from functions import *
+from gp_components import *
+
+terminals = [int_constant_factory(0, 10), Zero]
+functions = [Max, Min]
+
+tree = random_tree_full(3, functions, terminals)
+
+print(tree)
+print(tree.evaluate())
