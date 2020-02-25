d357fbb22ccbcabaed73d087761e04504d8782d9
RojerGS <rodrigogiraoserrao@gmail.com>
2020-02-25 11:12:24 +0000

    Include ProtectedDivision function

--- functions.py
+++ functions.py
@@ -46,3 +46,18 @@ class Product(BinaryFunction):
         self.evaluate_children(context, *args)
         return self.children_values[0] * self.children_values[1]
 
+class ProtectedDivision(BinaryFunction):
+    """Returns the protected division of the first children by the second.
+
+    The protected division of `a` with `b` is `a/b` if `b != 0`, else is `0`.
+    """
+    def __init__(self, children: List[ASTNode]):
+        super().__init__(children)
+
+    def evaluate(self, context = None, *args):
+        """If the right child is 0, return 0, otherwise do left/right."""
+        self.evaluate_children(context, *args)
+        if self.children_values[1]:
+            return self.children_values[0] / self.children_values[1]
+        else:
+            return 0
